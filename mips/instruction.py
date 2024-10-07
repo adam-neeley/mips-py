91b9e65cf0847c7c7aeda7ed242ad028671abac6
monadam <aneeley@gmail.com>
2024-10-07 10:18:27 -0700

    instruction

--- mips/instruction.py
+++ mips/instruction.py
@@ -1,4 +1,29 @@
+# class Execution:
+# def __init__(self, inst):  # , labels, machine, pc, flag, stack, ops):
+
+
+class Procedure:
+    """
+    A class to represent a procedure.
+    """
+
+    def __init__(self, machine, exec):
+        self.__machine = machine
+        self.__exec = exec
+
+    @property
+    def machine(self):
+        return self.__machine
+
+    @property
+    def exec(self):
+        return self.__exec
+
+
 class Instruction:
-    def __init__(self, text, proc):
-        self.text = text
+    "A class to represent an instruction."
+
+    def __init__(self, name, proc):
+        "Initialize an instruction."
+        self.name = name
         self.proc = proc
